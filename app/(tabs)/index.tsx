457c924c4a2acce051729d2a4e45ae3fb0409773
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-01-20 15:13:02 -0600

    bring in the image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,25 @@ import { View, StyleSheet } from "react-native";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
