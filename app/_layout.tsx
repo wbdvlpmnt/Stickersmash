183a769b328d8d0b2f8d312b296e12651b9a24d8
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-01-21 17:54:36 -0600

    configure the status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
 
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
